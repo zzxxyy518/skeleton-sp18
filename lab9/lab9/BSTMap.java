2545fa29a4f20355e822866cfac96c049fc32f00
Xingyu Zhou <u6887400@anu.edu.au>
2020-04-24 18:00:46 +0800

    finish part of lab9

--- lab9/lab9/BSTMap.java
+++ lab9/lab9/BSTMap.java
@@ -44,7 +44,16 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      *  or null if this map contains no mapping for the key.
      */
     private V getHelper(K key, Node p) {
-        throw new UnsupportedOperationException();
+        if(p != null){
+            if(p.key.equals(key)){
+                return p.value;
+            }
+            if(key.compareTo(p.key)<0){
+                return getHelper(key , p.left);
+            }else
+                return getHelper(key , p.right);
+        }
+        return null;
     }
 
     /** Returns the value to which the specified key is mapped, or null if this
@@ -52,7 +61,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        return getHelper(key , root);
     }
 
     /** Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -12,4 +12,26 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    private final static int WIDTH = 50;
+    private final static int HEIGHT = 50;
+    private static TERenderer TER;
+    private static TETile[][] WORLD = new TETile[WIDTH][HEIGHT];
+
+    private static void initializtion(){
+        TER.initialize(WIDTH,HEIGHT);
+        for(int x = 0; x< WIDTH; x++){
+            for(int y=0; y < HEIGHT; y++){
+                WORLD[x][y] = Tileset.NOTHING;
+            }
+        }
+    }
+
+    private static void addHexagon(int size,int x,int y){
+
+    }
+
+    public static void main(String[] args) {
+        initializtion();
+
+    }
 }

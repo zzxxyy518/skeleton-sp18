c55c2062d214b2da0452d7c474c92f909d850747
Xingyu Zhou <u6887400@anu.edu.au>
2020-04-27 17:26:14 +0800

    finish MyHashMap(Lab9)

--- lab9/lab9/MyHashMap.java
+++ lab9/lab9/MyHashMap.java
@@ -53,19 +53,32 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        if(key == null) throw new IllegalArgumentException("Give a null argument to get()");
+        int index = hash(key);
+        if(buckets[index].containsKey(key)){
+            return buckets[index].get(key);
+        }
+        return null;
     }
 
     /* Associates the specified value with the specified key in this map. */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        if(key == null) throw new IllegalArgumentException("Give a null argument to get()");
+        int index = hash(key);
+        if(!buckets[index].containsKey(key)) {
+            size++;
+            buckets[index].put(key, value);
+        }else{
+            buckets[index].remove(key);
+            buckets[index].put(key, value);
+        }
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////

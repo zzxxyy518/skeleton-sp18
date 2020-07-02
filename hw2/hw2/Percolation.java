14bcf124497b7b25fc3bd4d7f1b788f232b8f6b3
Xingyu Zhou <u6887400@anu.edu.au>
2020-07-02 19:16:46 +0800

    part of hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,27 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private boolean[][] mapStatus;
+
+
+    public Percolation(int N){
+        if(N <= 0) {
+            throw new IllegalArgumentException("Illegal N value");
+        }
+        mapStatus = new boolean[N][N];
+        for(int i = 0;i < N;i ++){
+            for(int j = 0;j < N;j ++){
+                mapStatus[i][j] = false;
+            }
+        }
+    }
+
+    public boolean isOpen(int row, int col){
+        if(row >= mapStatus.length || row < 0 || col >= mapStatus.length || col < 0){
+            throw new IndexOutOfBoundsException("Illegal coordinate for sites");
+        }
+        return mapStatus[row][col];
+    }
+
 
 }

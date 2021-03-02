3d21f3517b3cbf105e35f7b8db0d3b749b11011c
Xingyu Zhou <u6887400@anu.edu.au>
2021-03-02 18:19:05 +0800

    hw4 waiting for optimization

--- hw4/hw4/puzzle/Solver.java
+++ hw4/hw4/puzzle/Solver.java
@@ -2,18 +2,18 @@ package hw4.puzzle;
 
 
 import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Stack;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public final class Solver {
     public static class Node implements Comparable<Node> {
         WorldState item;
         int moves;
         Node prev;
-
-        public Node(WorldState state, int move, Node pre) {
-            item = state;
-            moves = move;
-            prev = pre;
-        }
 
         public Node(WorldState a) {
             item = a;
@@ -27,19 +27,64 @@ public final class Solver {
 
         @Override
         public int compareTo(Node o) {
-            return this.moves - o.moves;
+            return this.getItem().estimatedDistanceToGoal() + this.moves - o.getItem().estimatedDistanceToGoal() - o.moves;
         }
     }
 
-    public MinPQ<Node> res;
+    public MinPQ<Node> fringe;
+    public Node destination;
+    public Stack<WorldState> result;
+    public Set<Node> used;
+
+    public Node[] getNeighbors(Iterable<WorldState> neighbors) {
+        ArrayList<WorldState> mid = new ArrayList<>();
+        Iterator<WorldState> iterator = neighbors.iterator();
+        while (iterator.hasNext()) {
+            mid.add(iterator.next());
+        }
+        int size = mid.size();
+        Node[] res = new Node[size];
+        for (int i = 0; i < size; i++) {
+            res[i] = new Node(mid.get(i));
+        }
+        return res;
+    }
 
     public Solver(WorldState initial) {
-        res = new MinPQ<>();
-        res.insert(new Node(initial));
+        fringe = new MinPQ<>();
+        result = new Stack<>();
+        used = new HashSet<>();
+        int num = 1;
+        fringe.insert(new Node(initial));
+        used.add(new Node(initial));
+        while (!fringe.min().getItem().isGoal()) {
+            Node temp = fringe.delMin();
+            for (WorldState worldState : temp.getItem().neighbors()) {
+                Node mid = new Node(worldState);
+                if (used.contains(mid)) {
+                    continue;
+                }
+                mid.prev = temp;
+                mid.moves = mid.prev.moves +1;
+                fringe.insert(mid);
+                used.add(mid);
+                num ++;
+            }
+        }
+        destination = fringe.min();
+        while(destination.prev != null) {
+            result.push(destination.getItem());
+            destination = destination.prev;
+        }
+        result.push(destination.getItem());
+        System.out.println("number :" + num);
     }
 
     public int moves() {
-        Node mid = res.delMin();
-        int a =
+        return result.size() - 1;
+    }
+
+    public Iterable<WorldState> solution() {
+        return result;
     }
 }

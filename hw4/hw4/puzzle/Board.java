3f9df46b624d01c67f33cba80e773cdd7e9df33e
Xingyu Zhou <u6887400@anu.edu.au>
2021-03-04 15:55:04 +0800

    finish hw4 without optimization

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -1,21 +1,132 @@
 package hw4.puzzle;
 
-public class Board {
+import edu.princeton.cs.algs4.Queue;
 
-    /** Returns the string representation of the board. 
-      * Uncomment this method. */
- /*   public String toString() {
+public class Board implements WorldState {
+
+    /**
+     * Returns the string representation of the board.
+     * Uncomment this method.
+     */
+    private int[][] tiles;
+    private final static int BLANK = 0;
+    private final int N;
+
+    public Board(int[][] tiles) {
+        N = tiles.length;
+        this.tiles = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            System.arraycopy(tiles[i], 0, this.tiles[i], 0, N);
+        }
+    }
+
+    public String toString() {
         StringBuilder s = new StringBuilder();
         int N = size();
         s.append(N + "\n");
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                s.append(String.format("%2d ", tileAt(i,j)));
+                s.append(String.format("%2d ", tileAt(i, j)));
             }
             s.append("\n");
         }
         s.append("\n");
         return s.toString();
     }
-*/
+
+    public int tileAt(int i, int j) {
+        if (i < 0 || i > N - 1 || j < 0 || j > N - 1) {
+            throw new IndexOutOfBoundsException("invalid i or j out of the board");
+        }
+        return tiles[i][j];
+    }
+
+    public int size() {
+        return N;
+    }
+
+    @Override
+    public int estimatedDistanceToGoal() {
+        return manhattan();
+    }
+
+    /**
+     * @return the neighbors of this board
+     * @author SPOILERZ (Cited from http://joshh.ug/neighbors.html)
+     */
+    public Iterable<WorldState> neighbors() {
+        Queue<WorldState> neighbors = new Queue<>();
+        int hug = size();
+        int bug = -1;
+        int zug = -1;
+        for (int rug = 0; rug < hug; rug++) {
+            for (int tug = 0; tug < hug; tug++) {
+                if (tileAt(rug, tug) == BLANK) {
+                    bug = rug;
+                    zug = tug;
+                }
+            }
+        }
+        int[][] ili1li1 = new int[hug][hug];
+        for (int pug = 0; pug < hug; pug++) {
+            for (int yug = 0; yug < hug; yug++) {
+                ili1li1[pug][yug] = tileAt(pug, yug);
+            }
+        }
+        for (int l11il = 0; l11il < hug; l11il++) {
+            for (int lil1il1 = 0; lil1il1 < hug; lil1il1++) {
+                if (Math.abs(-bug + l11il) + Math.abs(lil1il1 - zug) - 1 == 0) {
+                    ili1li1[bug][zug] = ili1li1[l11il][lil1il1];
+                    ili1li1[l11il][lil1il1] = BLANK;
+                    Board neighbor = new Board(ili1li1);
+                    neighbors.enqueue(neighbor);
+                    ili1li1[l11il][lil1il1] = ili1li1[bug][zug];
+                    ili1li1[bug][zug] = BLANK;
+                }
+            }
+        }
+        return neighbors;
+    }
+
+    public int hamming() {
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (tiles[i][j] == BLANK) {
+                    continue;
+                }
+                int locNo = i * N + j + 1;
+                if (locNo != tiles[i][j]) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+
+    public int manhattan() {
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (tiles[i][j] == BLANK) {
+                    continue;
+                }
+                int row = (tiles[i][j] - 1) / N;
+                int col = (tiles[i][j] - 1) % N;
+                result += Math.abs(row - i) + Math.abs(col - j);
+            }
+        }
+        return result;
+    }
+
+    public boolean equals(Object y) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (tiles[i][j] != ((Board) y).tileAt(i, j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }

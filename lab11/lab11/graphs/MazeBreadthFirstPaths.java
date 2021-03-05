dece6871a89388dbd2f24a5de85a0b2d7bead44c
Xingyu Zhou <u6887400@anu.edu.au>
2021-03-05 19:21:06 +0800

    finish lab11

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,7 +1,10 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Queue;
+
 /**
- *  @author Josh Hug
+ * @author Josh Hug
  */
 public class MazeBreadthFirstPaths extends MazeExplorer {
     /* Inherits public fields:
@@ -9,21 +12,51 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    private boolean targetFound = false;
+    private Maze maze;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
-    /** Conducts a breadth first search of the maze starting at the source. */
-    private void bfs() {
+    /**
+     * Conducts a breadth first search of the maze starting at the source.
+     */
+    private void bfs(int v) {
         // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        Queue<Integer> fringe = new Queue<>();
+        fringe.enqueue(v);
+        marked[v] = true;
+        announce();
+        while (!fringe.isEmpty()) {
+            int temp = fringe.dequeue();
+            for(int adj : maze.adj(temp)) {
+                if (!marked[adj]) {
+                    marked[adj] = true;
+                    edgeTo[adj] = temp;
+                    distTo[adj] = distTo[temp] + 1;
+                    announce();
+                    if (adj == t) {
+                        return;
+                    }
+                    fringe.enqueue(adj);
+                }
+            }
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+         bfs(s);
     }
 }
 
